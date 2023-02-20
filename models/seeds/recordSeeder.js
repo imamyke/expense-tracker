2ed742d4630aa30c678277817404734e004779e6
Amy Ke <xiahamy@gmail.com>
2023-02-20 22:57:33 +0800

    modify recordSeeder.js

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,11 +1,25 @@
+const Record = require('../record') 
+const User = require('../user')
+const Category = require('../category')
 const bcrypt = require('bcryptjs')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const Record = require('../record') // Schema
-const User = require('../user')
-const Category = require('../category')
 
+// 產生類別種子資料
+const CATEGORY = {
+  家居物業: "fa-solid fa-house",
+  交通出行: "fa-solid fa-van-shuttle",
+  休閒娛樂: "fa-solid fa-face-grin-beam",
+  餐飲食品: "fa-solid fa-utensils",
+  其他: "fa-solid fa-pen"
+}
+const categoryItems = Object.entries(CATEGORY)
+for (let category of categoryItems) {
+  Category.create({ name: category[0], icon: category[1] })
+}
+
+// 產生使用者
 let SEED_USER = []
 for (let i = 1; i < 3; i++) {
   const users = {
@@ -16,6 +30,7 @@ for (let i = 1; i < 3; i++) {
   SEED_USER.push(users)
 }
 
+// 產生 default users
 const date = new Date()
 let SEED_RECORD = {
   name: '晚餐',
@@ -24,45 +39,44 @@ let SEED_RECORD = {
   category: '餐飲食品'
 }
 
+// 產生使用者及 record 種子資料
 let userId = ''
 let categoryId = ''
-
-Category.find()
-  .lean()
-  .then(categories => {
-    const selectedCategory = categories.find(category => category.name === SEED_RECORD.category)
-    categoryId = selectedCategory._id
-  })
-  
-// Mongoose(資料庫)連線 => 產生種子資料
 const db = require('../../config/mongoose')
 db.once('open', () => {
-  const recordSeeder = (user, record) => {
-    bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(user.password, salt))
-    .then(hash => User.create({
-      name: user.name,
-      email: user.email,
-      password: hash
-    }))
-    .then(user => {
-      userId = user._id
-      console.log(userId);
-    })
-    .then(() => {
-      return Promise.all(Array.from(
-        { length: 3 },
-        (_, i) => Record.create({ ...record, userId, categoryId })
-      ))
-    })
-    .then(() => {
-      console.log('done.')
+  new Promise((resolve, _reject) => {
+    Category.find()
+      .lean()
+      .then(categories => {
+        const selectedCategory = categories.find(category => category.name === SEED_RECORD.category)
+        categoryId = selectedCategory._id
+        return categoryId
+      }).then(() => {
+        for (const [user_index, user] of Object.entries(SEED_USER) ) {
+          console.log(user_index, user);
+          bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(user.password, salt))
+          .then(hash => User.create({
+            name: user.name,
+            email: user.email,
+            password: hash
+          }))
+          .then(user => {
+            userId = user._id
+            console.log(userId);
+            return Record.create({ ...SEED_RECORD, userId, categoryId })
+          })
+          .then(() => {
+            console.log('record done')
+            resolve()
+          })
+        }
+      })
+    }).then(() => {
+      console.log('Yes done.')
       process.exit()
     })
-  }
-
-  recordSeeder(SEED_USER[0], SEED_RECORD)
 })
 
 

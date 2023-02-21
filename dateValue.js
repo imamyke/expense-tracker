40b1aea780c67df6dc283309b6416a8aae4de257
Amy Ke <xiahamy@gmail.com>
2023-02-21 21:24:36 +0800

    modify expense.js & dateValue.js

--- dateValue.js
+++ dateValue.js
@@ -2,7 +2,7 @@ const dateValue = (date) => {
   let day = date.getDate()
   let mounth = date.getMonth() + 1
   let year = date.getFullYear()
-  let formattedDate = ''
+  let dateValue = ''
 
   if (day < 10) {
     day = '0'+ day
@@ -11,8 +11,8 @@ const dateValue = (date) => {
     mounth = '0' + mounth
   }
 
-  formattedDate = year + '-' + mounth + '-' + day
-  return formattedDate
+  dateValue = year + '-' + mounth + '-' + day
+  return dateValue
 }
 
 module.exports = dateValue
--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -16,25 +16,16 @@ router.post('/', (req, res) => {
     .then(categories => {
       const record = req.body
       const errors = []
-      const targetCategory = categories.find(category => category.name === record.category)
-      if (!record.name || !record.date || !targetCategory || !record.amount) {
-        // 導入類別
-        const selected = {
-            '家居物業': false,
-            '交通出行': false,
-            '休閒娛樂': false,
-            '餐飲食品': false,
-            '其他': false
-          }
-        const keySelected = Object.keys(selected).find(key => key === record.category)
+      const keySelected = Object.entries(categories).find(category => category[1].name === record.category)
+      if (!record.name || !record.date || !keySelected || !record.amount) {
         if (!keySelected) {
           errors.push({ message: '請確認欄位已確實填寫完畢!' })
           return res.render('new', { record , errors, keySelected: false })
         }
         errors.push({ message: '請確認欄位已確實填寫完畢!' })
-        return res.render('new', { record , errors, keySelected })
+        return res.render('new', { record , errors, keySelected: keySelected[1].name })
       }
-      categoryId = targetCategory._id
+      categoryId = keySelected[1]._id
       return Record.create({ ...record, categoryId, userId })
         .then(() => res.redirect('/'))
         .catch(error => console.log(error))
@@ -52,44 +43,33 @@ router.get('/:id/edit', (req, res) => {
       const recordDate = dateValue(record.date)
       record.date = recordDate
       // 導入類別
-      const selected = {
-            '家居物業': false,
-            '交通出行': false,
-            '休閒娛樂': false,
-            '餐飲食品': false,
-            '其他': false
-          }
-      const keySelected = Object.keys(selected).find(key => key === record.category)
-      res.render('edit', { _id , record, keySelected })
+      Category.find()
+        .lean()
+        .then(categories => {
+          const keySelected = Object.entries(categories).find(category => category[1].name === record.category)
+          res.render('edit', { _id , record, keySelected: keySelected[1].name })
+        })
+        .catch(error => console.log(error))
     })
     .catch(error => console.log(error))
 })
 router.put('/:id', (req, res) => {
   const _id = req.params.id 
   const userId = req.user._id
-
   Category.find()
       .lean()
       .then(categories => {
           const record = req.body
           const errors = []
-          const targetCategory = categories.find(category => category.name === record.category)
-          if (!record.name || !record.date || !targetCategory || !record.amount) {
+          const keySelected = Object.entries(categories).find(category => category[1].name === record.category)
+          if (!record.name || !record.date || !keySelected || !record.amount) {
             // 導入類別
-            const selected = {
-              '家居物業': false,
-              '交通出行': false,
-              '休閒娛樂': false,
-              '餐飲食品': false,
-              '其他': false
-            }
-            const keySelected = Object.keys(selected).find(key => key === record.category)
             if (!keySelected) {
               errors.push({ message: '請確認欄位已確實填寫完畢!' })
               return res.render('edit', { _id , record, errors, keySelected: false })
             }
             errors.push({ message: '請確認欄位已確實填寫完畢!' })
-            return res.render('edit', { _id ,record, errors, keySelected })
+            return res.render('edit', { _id ,record, errors, keySelected: keySelected[1].name })
           }
         return Record.findOneAndUpdate({ _id, userId }, record)
                 .then(() => res.redirect('/'))
